6e375da916fe96c155e35c7a5e8a7a838f9523c3
achiu08 <114778512+achiu08@users.noreply.github.com>
2022-11-14 14:50:35 -0800

    Add files via upload
    Chart 1 - pie chart

---
+++ docs/chart_for_A2.R
@@ -0,0 +1,15 @@
+shows <- read.csv("https://raw.githubusercontent.com/info201a-au2022/project-group-1-section-ag/main/data/netflix_titles.csv", stringsAsFactors = FALSE)
+
+library(ggplot2)
+library(tidyverse)
+
+listed <- shows %>% 
+  filter (type == "TV Show") %>% 
+  filter(country == "United States") %>% 
+  group_by(listed_in) %>% 
+  filter(release_year > 2018) 
+
+ggplot(data= listed, aes(x= "", y= listed_in, fill= release_year)) +
+  geom_bar(stat= "identity", width = 1) +
+  coord_polar("y", start= 0)
+
