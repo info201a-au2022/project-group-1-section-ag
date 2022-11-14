958aba158e5c01a15085e69095e741a2e1e80d8a
Emily Ma <114948378+emilywma@users.noreply.github.com>
2022-11-14 15:50:04 -0800

    Add files via upload

---
+++ source/chart3.R
@@ -0,0 +1,15 @@
+netflix <- read.csv("https://raw.githubusercontent.com/info201a-au2022/project-group-1-section-ag/main/data/n_movies.csv")
+View(netflix)
+
+library(tidyverse)
+install.packages("ggplot2")
+library("ggplot2")
+
+top_IMBD <-netflix %>%
+  filter(str_detect(netflix$genre, "Action, Crime")) %>%
+  arrange(desc(rating)) %>%
+  slice(1:10)
+view(top_IMBD)
+
+ggplot(data = top_IMBD) +
+  geom_col(mapping = aes(x= title, y= rating, fill = title  ))
