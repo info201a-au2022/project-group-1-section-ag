f6c021102c9c81928a8d0976a07316cc406fa293
ianpascal <114777784+ianpascal@users.noreply.github.com>
2022-12-02 20:18:24 -0800

    Update netflix_map.R

--- source/netflix_map.R
+++ source/netflix_map.R
@@ -6,11 +6,8 @@ library(dplyr)
 df_netflix_titles <- read_csv("https://raw.githubusercontent.com/info201a-au2022/project-group-1-section-ag/main/data/netflix_titles.csv")
 
 df_netflix_titles <- df_netflix_titles %>%
-  mutate(main_country = map(str_split(country, ", "), 1), 
-         main_cast = map(str_split(cast, ", "), 1), 
-         genre = map(str_split(listed_in, ", "), 1)) %>% 
-  unnest(cols = c(main_country, main_cast, genre)) %>% 
-  mutate(type = as.factor(type)
+  mutate(main_country = map(str_split(country, ", "), 1) %>%
+  unnest(cols = c(main_country))
   ) 
 head(df_netflix_titles)
 
@@ -26,19 +23,16 @@ netflix_for_map <- df_netflix_titles %>%
                                           "Soviet Union" = "Russia",
                                           "West Germany" = "Germany")))
 
-#summarize content counts by country
 count_country <- netflix_for_map %>% 
   group_by(main_country) %>% 
   summarise(content_count = n()) %>% 
   ungroup() %>% 
   arrange(desc(content_count))
 
-#join map data with the dataframe
 map_join <- mapdata %>% 
   left_join(. , count_country, by = c("region"="main_country")) %>% 
   mutate(content_count = replace_na(content_count, 0))
 
-# creating chroloplete map
 temp <- ggplot() +
   geom_polygon(data = map_join, 
                aes(fill = content_count, x = long, y = lat, 
