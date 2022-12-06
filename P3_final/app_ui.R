8626edaf2fcb2c81bf06dc666dcb98f0507c82f6
ianpascal <114777784+ianpascal@users.noreply.github.com>
2022-12-05 17:31:08 -0800

    Update app_ui.R

--- P3_final/app_ui.R
+++ P3_final/app_ui.R
@@ -3,3 +3,23 @@
 map_main_content <- mainPanel(
   plotlyOutput("map")
 )
+
+map_panel <- tabPanel(
+  "Map",
+  titlePanel("Electoral College Votes"),
+  # A `sidebarLayout()` that contains...
+  sidebarLayout(
+    # Your `map_sidebar_content`
+    map_sidebar_content,
+    
+    # Your `map_main_content`
+    map_main_content
+  )
+)
+
+
+ui <- navbarPage(
+  "Netlix Movies and Shows",
+  map_panel, 
+  scatter_panel
+)
