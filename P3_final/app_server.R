3bf40b2f127b0514c2d5490e91639414faf2488e
ianpascal <114777784+ianpascal@users.noreply.github.com>
2022-12-05 16:06:02 -0800

    Update app_server.R

--- P3_final/app_server.R
+++ P3_final/app_server.R
@@ -2,7 +2,11 @@ library(ggplot2)
 library(plotly)
 library(dplyr)
 
-server <- function(input, output) {
+df_netflix_titles <- read_csv("https://raw.githubusercontent.com/info201a-au2022/project-group-1-section-ag/main/data/netflix_titles.csv")
 
+server <- function(input, output) {
+    output$map <- renderPlotly({ 
+      
+  }) 
 
 }
