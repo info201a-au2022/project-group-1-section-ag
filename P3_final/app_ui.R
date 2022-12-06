23cd434f77f6a49377728eb5884dc67bafefe5a7
ianpascal <114777784+ianpascal@users.noreply.github.com>
2022-12-06 15:08:16 -0800

    Update app_ui.R

--- P3_final/app_ui.R
+++ P3_final/app_ui.R
@@ -2,17 +2,38 @@ map_main_content <- mainPanel(
   plotlyOutput("map")
 )
 
+imdb_main_content <- mainPanel(
+  plotlyOutput("imdb")
+)
+
 map_panel <- tabPanel(
   "Map",
   titlePanel("Choropleth map"),
-    
-    map_main_content
-  )
-
-
-
-ui <- navbarPage(
-  "Netlix Movies and Shows",
-  map_panel
+  
+  map_main_content
+  
 )
 
+imdb_sidebar_content <- sidebarPanel(
+  
+  selectInput(
+    "var",
+    label = "Genre imdb rating",
+    choices = list(
+      "Action Content" = "Action",
+      "Comedy Content" = "Comedy",
+      "Drama Content" = "Drama"
+    )
+  )
+)
+
+imdb_panel <- tabPanel(
+  "imdb",
+  titlePanel("Netflix Top IMDB Content"),
+  sidebarLayout(
+
+    imdb_sidebar_content,
+    
+    imdb_main_content
+  )
+)
