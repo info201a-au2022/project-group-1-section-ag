834a1ff4e18fb489cf3ec6becacb4f9a752fe5f8
ianpascal <114777784+ianpascal@users.noreply.github.com>
2022-12-05 22:01:42 -0800

    Update app_ui.R

--- P3_final/app_ui.R
+++ P3_final/app_ui.R
@@ -1,5 +1,3 @@
-#
-
 map_main_content <- mainPanel(
   plotlyOutput("map")
 )
@@ -8,14 +6,13 @@ map_panel <- tabPanel(
   "Map",
   titlePanel("Choropleth map"),
     
-    # Your `map_main_content`
     map_main_content
   )
-)
+
 
 
 ui <- navbarPage(
   "Netlix Movies and Shows",
-  map_panel, 
-  scatter_panel
+  map_panel
 )
+
