8262af186ed18486c38270f17a90528200024957
ianpascal <114777784+ianpascal@users.noreply.github.com>
2022-12-06 20:54:16 -0800

    Update chart_2.R

--- source/chart_2.R
+++ source/chart_2.R
@@ -7,6 +7,25 @@ install.packages("ggplot2") # once per machine
 library("ggplot2")          # in each relevant script
 
 
+build_graph <- function(data, type){
+  top_IMBD <- data %>%
+    filter(str_detect(netflix_filter_imdb$genre, type)) %>%
+    arrange(desc(rating)) %>%
+    slice(1:10)
+  
+  chart_3 <- ggplot(top_IMBD, aes(x=title, y=rating, fill=rating)) + 
+    geom_bar(stat="identity") +
+    geom_text(aes(label=rating), vjust=-0.3, size=3.5) +
+    coord_flip() +
+    labs(
+      title = "Top 10 IMDB rated content on Netflix",
+      x = "Content Titles",
+      y = "IMDB rating"
+    )
+  
+  return(chart_3)
+}
+
 
 filtered_tv_netflix <- netflix %>%
   filter(str_detect(netflix$certificate, "TV"))
